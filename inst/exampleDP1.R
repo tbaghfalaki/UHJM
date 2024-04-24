23b4e1163e0571f5136e061a104961c58cefd5f1
Taban Baghfalaki <64243070+tbaghfalaki@users.noreply.github.com>
2024-04-24 15:18:33 +0200

    new

--- inst/exampleDP1.R
+++ inst/exampleDP1.R
@@ -32,7 +32,7 @@ rm(list=ls())
     formSurv = Surv(survtime, death) ~ w1 + w2,
     dataLong = dataLong_t, dataSurv = dataSurv_t,
     obstime = "obstime", id = "id", n.chains = 2,
-    n.iter = 200, n.burnin = 100, n.thin = 1, K = 15, family = "NB"
+    n.iter = 2000, n.burnin = 1000, n.thin = 1, K = 15, family = "NB"
   )
 
 
